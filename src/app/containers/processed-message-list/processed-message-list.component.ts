f56823bf1cf1eca7fec2b0c82ffa4462f66dcf74
scottlee309 <scottlee309@gmail.com>
2017-02-12 14:27:55 +1100

    completed processes messages component for birthday wish messages

--- src/app/containers/processed-message-list/processed-message-list.component.ts
+++ src/app/containers/processed-message-list/processed-message-list.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import * as fromRoot from '../../reducers';
+import { ProcessedMessage} from '../../models/processed-message.class';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'mp-processed-message-list',
   templateUrl: './processed-message-list.component.html',
   styleUrls: ['./processed-message-list.component.scss']
 })
 export class ProcessedMessageListComponent {
+  birthdayWishMessages$: Observable<ProcessedMessage[]>;
+  congratulationsOnBabyMessages$: Observable<ProcessedMessage[]>;
 
-
+  constructor(private store: Store<fromRoot.State>) {
+    this.birthdayWishMessages$ = this.store.select(fromRoot.getProcessedBirthdayWishMessages);
+    this.congratulationsOnBabyMessages$ = this.store.select(fromRoot.getProcessedCongratulationsOnBabyMessages);
+  }
 }
